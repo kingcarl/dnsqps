59740da64813292b4542ef738ba9dcc3f4e1ba28
kingcarl <kingcarl725@yahoo.com>
2013-09-21 13:05:40 +0800

    add dst ip filter
    add dst ip filter

--- dnsqps/dnsqps.c
+++ dnsqps/dnsqps.c
@@ -31,6 +31,9 @@
 
 #include <pcap.h>
 
+// thanks dnstop's authors
+#include "inX_addr.h" 
+
 #define PCAP_SNAPLEN 65535
 #define MAX_QNAME_SZ 512
 #define DNS_MSG_HDR_SZ 12
@@ -62,11 +65,11 @@ unsigned int query_count_intvl = 0;
 unsigned int reply_count_intvl = 0;
 int opt_count_queries = 0;
 int opt_count_replies = 0;
+int dst_flag = 0;
 
 int (*handle_datalink) (const u_char * pkt, int len)= NULL;
 
-typedef struct _rfc1035_header rfc1035_header;
-struct _rfc1035_header {
+typedef struct _rfc1035_header {
     unsigned short id;
     unsigned int qr:1;
     unsigned int opcode:4;
@@ -79,10 +82,69 @@ struct _rfc1035_header {
     unsigned short ancount;
     unsigned short nscount;
     unsigned short arcount;
-};
+} rfc1035_header;
+
+typedef struct ip_list_d {
+    inX_addr addr;
+    void *data;
+    struct ip_list_d *next;
+} ip_list_t;
+
+ip_list_t *Attachlist;
 
 int
-handle_dns(const char *buf, int len)
+attach_list_match(const inX_addr *addr)
+{
+    ip_list_t *ptr;
+    
+    for (ptr = Attachlist; ptr != NULL; ptr = ptr->next){
+	if (0 == inXaddr_cmp(addr, &ptr->addr))
+	    return 0;
+    }
+    return 1;
+}
+
+void
+attach_list_add(const inX_addr *addr)
+{
+    ip_list_t *new;
+    if (attach_list_match(addr) == 0)
+	return;
+    
+    new = malloc(sizeof(ip_list_t));
+    if (new == NULL){
+	perror("malloc");
+	return;
+    }
+    
+    new->addr = *addr;
+    new->next = Attachlist;
+    Attachlist = new;
+}
+
+void
+attach_dst_addr(const char *name)
+{
+    struct addrinfo *ai_list;
+    struct addrinfo *ai_ptr;
+    struct inX_addr *addr;
+    int status;
+    status = getaddrinfo(name, NULL, NULL, &ai_list);
+    if (status != 0 )
+	return ;
+    
+    for (ai_ptr = ai_list; ai_ptr != NULL; ai_ptr = ai_ptr->ai_next){
+	if (ai_ptr->ai_family == AF_INET){
+	    inXaddr_assign_v4(&addr, &((struct sockaddr_in *)ai_ptr->ai_addr)->sin_addr);
+	    attach_list_add(&addr);
+	}
+    }
+    
+    freeaddrinfo(ai_list);
+}
+
+int
+handle_dns(const char *buf, int len, const inX_addr *dst_addr)
 {
     rfc1035_header qh;
     unsigned short us;
@@ -102,16 +164,18 @@ handle_dns(const char *buf, int len)
     if (1 == qh.qr && 0 == opt_count_replies)
 	return 0;
 
-    if (0 == qh.qr) {
+    if (0 == qh.qr && 0 == attach_list_match(dst_addr)) {
 	query_count_intvl++;
-    } else {
+    } else if (1 == qh.qr) {
 	reply_count_intvl++;
+    } else {
+	return 1;
     }
     return 1;
 }
 
 int
-handle_tcp(const struct tcphdr *tcp, int len)
+handle_tcp(const struct tcphdr *tcp, int len, const inX_addr *dst_addr)
 {
     
 /*
@@ -154,7 +218,7 @@ struct tcphdr {
     if (check_port && check_port != tcp->dest && check_port != tcp->source)
 	return 0;
     
-    if (tcp->fin == 1 || tcp->syn == 1 || tcp->rst == 1 || (tcp->ack == 1 && tcp->psh == 0))
+    if (tcp->psh == 0)
         return 0;
     
     if (0 == handle_dns((char *)(tcp + 1), len - sizeof(*tcp)))
@@ -163,11 +227,11 @@ struct tcphdr {
 }
 
 int
-handle_udp(const struct udphdr *udp, int len)
+handle_udp(const struct udphdr *udp, int len, const inX_addr *dst_addr)
 {
     if (check_port && check_port != udp->uh_dport && check_port != udp->uh_sport)
 	return 0;
-    if (0 == handle_dns((char *)(udp + 1), len - sizeof(*udp)))
+    if (0 == handle_dns((char *)(udp + 1), len - sizeof(*udp), dst_addr))
 	return 0;
     return 1;
 }
@@ -176,15 +240,19 @@ int
 handle_ipv4(const struct ip *ip, int len)
 {
     int offset = ip->ip_hl << 2;
+    inX_addr dst_addr;
+    
+    inXaddr_assign_v4(&dst_addr, &ip->ip_dst);
+    
     if (IPPROTO_UDP == ip->ip_p)
     {
-	if (0 == handle_udp((struct udphdr *)((char *)ip + offset), len - offset))
+	if (0 == handle_udp((struct udphdr *)((char *)ip + offset), len - offset, &dst_addr))
 	    return 0;
 	return 1;
     }
     else if (IPPROTO_TCP == ip->ip_p)
     {
-	if (0 == handle_tcp((struct tcphdr *)((char *)ip + offset), len - offset))
+	if (0 == handle_tcp((struct tcphdr *)((char *)ip + offset), len - offset, &dst_addr))
 	    return 0;
 	return 1;
     }
@@ -230,7 +298,8 @@ handle_pcap(u_char * udata, const struct pcap_pkthdr *hdr, const u_char * pkt)
 	return;
 }
 
-void prompt_info(int signo)
+void
+prompt_info(int signo)
 {
     if(1 == opt_count_queries && 0 == opt_count_replies)
     {
@@ -246,8 +315,9 @@ void prompt_info(int signo)
 	printf("%d rps\n", reply_count_intvl);
     }
     
+    free(Attachlist);
     pcap_close(pcap);
-	exit(0);	
+    exit(0);	
 }
 
 void init_sigaction(void)
@@ -271,9 +341,10 @@ void init_time(void)
 void
 usage(void)
 {
-    fprintf(stderr, "usage: %s [opts] netdevice\n", progname);
+    fprintf(stderr, "usage: %s [opts] [-d x.x.x.x]  netdevice\n", progname);
     fprintf(stderr, "\t-Q\tCount queries\n");
     fprintf(stderr, "\t-R\tCount responses\n");
+    fprintf(stderr, "\t-d\tCapture Destination IP Address \n");
     fprintf(stderr, "\t-v\tshow version information\n");
     fprintf(stderr, "\t-h\tshow help information\n");
     exit(1);
@@ -288,13 +359,17 @@ main(int argc, char *argv[])
 
     progname = strdup(strrchr(argv[0], '/') ? strchr(argv[0], '/') + 1 : argv[0]);
 
-    while ((x = getopt(argc, argv, "hQRv")) != -1) {
+    while ((x = getopt(argc, argv, "hd:QRv")) != -1) {
 	switch (x) {
 	case 'Q':
 	    opt_count_queries = 1;
 	    break;
 	case 'R':
 	    opt_count_replies = 1;
+	    break;
+	case 'd':
+	    dst_flag = 1;
+	    attach_dst_addr(optarg);
 	    break;
 	case 'v':
 	    fprintf(stderr, "dnsqps Version: %s\n", Version);
@@ -310,7 +385,7 @@ main(int argc, char *argv[])
     argc -= optind;
     argv += optind;
 
-    if (argc < 1)
+    if (argc < 1 && 0 == dst_flag)
 	usage();
     device = strdup(argv[0]);
 	
